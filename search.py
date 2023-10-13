4cd40a55183017a6c04267c17ac39aead0b296ae
hhiieepp <145389759+hhiieepp@users.noreply.github.com>
2023-10-13 22:37:37 +0700

    22021169 Nguyen Ngoc Hiep

--- search.py
+++ search.py
@@ -19,6 +19,7 @@ Pacman agents (in searchAgents.py).
 
 import util
 
+
 class SearchProblem:
     """
     This class outlines the structure of a search problem, but doesn't implement
@@ -70,7 +71,8 @@ def tinyMazeSearch(problem):
     from game import Directions
     s = Directions.SOUTH
     w = Directions.WEST
-    return  [s, s, w, s, w, w, s, w]
+    return [s, s, w, s, w, w, s, w]
+
 
 def depthFirstSearch(problem: SearchProblem):
     """
@@ -87,17 +89,110 @@ def depthFirstSearch(problem: SearchProblem):
     print("Start's successors:", problem.getSuccessors(problem.getStartState()))
     """
     "*** YOUR CODE HERE ***"
-    util.raiseNotDefined()
+    # Storing the intial state
+    initialState = problem.getStartState()
+    # Initializing the frontier as a stack since we are in DFS
+    frontier = util.Stack()
+    # Push the initial state to the stack
+    frontier.push(initialState)
+    # Initialzing the explored set as set
+    explored = set()
+    # A dictionary to store for every state its parent and action to get there
+    # I'm assuming that the initial state has none of the above
+    nodeParentAction = {initialState: (None, None)}
+    # The loop is executing until the stack is empty and then return an empty list
+    # Or until a goal state is found
+    while not frontier.isEmpty():
+        # Pop the node of the frontier
+        node = frontier.pop()
+        # Checking if it a goal state
+        if problem.isGoalState(node):
+            # If it is, initialize a list a the path then execute
+            # A simple algorithm to construct the path to the goal state
+            # For every node i get the action and store it to front of the list because
+            # I started from the goal state and needs to go to the start state
+            path = []
+            while nodeParentAction[node][0] != None:
+                path[:0] = [nodeParentAction[node][1]]
+                node = nodeParentAction[node][0]
+            return path
+        # If not add the node in the explored set
+        explored.add(node)
+        # Start a checking process for all the childs of the current node
+        for child, action, cost in problem.getSuccessors(node):
+            # If the child has not been visited before
+            # Add it to the frontier and also make a registry for the dictionary 
+            if child not in explored:
+                nodeParentAction[child] = (node, action)
+                frontier.push(child)
+    return []
+
 
 def breadthFirstSearch(problem: SearchProblem):
     """Search the shallowest nodes in the search tree first."""
     "*** YOUR CODE HERE ***"
-    util.raiseNotDefined()
+    # Most of the algorithm is the same as the DFS so i skipped the same comments 
+    # And prefered to highlight only the key differences
+    initialState = problem.getStartState()
+    # Initializing the frontier as a stack since we are in BFS
+    frontier = util.Queue()
+    frontier.push(initialState)
+    explored = set()
+    nodeParentAction = {initialState: (None, None)}
+    while not frontier.isEmpty():
+        node = frontier.pop()
+        if problem.isGoalState(node):
+            path = []
+            while nodeParentAction[node][0] != None:
+                path[:0] = [nodeParentAction[node][1]]
+                node = nodeParentAction[node][0]
+            return path
+        explored.add(node)
+        for child, action, cost in problem.getSuccessors(node):
+            # If the child has not been visited before
+            # And it's not inside the queue, which can be checked 
+            # By checking if there is a registry for that node in the dictionary
+            # Add it to the frontier and also make a registry for the dictionary 
+            if child not in (explored and nodeParentAction):
+                nodeParentAction[child] = (node, action)
+                frontier.push(child)
+    return []
+
 
 def uniformCostSearch(problem: SearchProblem):
     """Search the node of least total cost first."""
     "*** YOUR CODE HERE ***"
-    util.raiseNotDefined()
+    # Most of the algorithm is the same as the previous so i skipped the same comments 
+    # And prefered to highlight only the key differences
+    initialState = problem.getStartState()
+    # Initializing the frontier as a priority queue since we are in UCS
+    frontier = util.PriorityQueue()
+    frontier.push(initialState, 0)
+    explored = set()
+    nodeParentActionCost = {initialState: (None, None, 0)}
+    while not frontier.isEmpty():
+        node = frontier.pop()
+        if problem.isGoalState(node):
+            path = []
+            while nodeParentActionCost[node][0] != None:
+                path[:0] = [nodeParentActionCost[node][1]]
+                node = nodeParentActionCost[node][0]
+            return path
+        explored.add(node)
+        for child, action, cost in problem.getSuccessors(node):
+            # If the child has not been visited before
+            # Then if it is not inside the frontier just add it and make a registry in the dictionary
+            # But if it is then check if the cost that the previous path to the child node is bigger than the new one
+            # If it is then update the frontier with the new priority and also update the registry in the dictionary
+            if child not in explored:
+                if child not in nodeParentActionCost:
+                    nodeParentActionCost[child] = (node, action, nodeParentActionCost[node][2] + cost)
+                    frontier.push(child, nodeParentActionCost[node][2] + cost)
+                elif nodeParentActionCost[node][2] + cost < nodeParentActionCost[child][2]:
+                    nodeParentActionCost[child] = (node, action, nodeParentActionCost[node][2] + cost)
+                    frontier.update(child, nodeParentActionCost[node][2] + cost)
+    return []
+
 
 def nullHeuristic(state, problem=None):
     """
@@ -106,10 +201,37 @@ def nullHeuristic(state, problem=None):
     """
     return 0
 
+
 def aStarSearch(problem: SearchProblem, heuristic=nullHeuristic):
     """Search the node that has the lowest combined cost and heuristic first."""
     "*** YOUR CODE HERE ***"
-    util.raiseNotDefined()
+    # Most of the algorithm is the same as the previous so i skipped the same comments 
+    # And prefered to highlight only the key differences
+    initialState = problem.getStartState()
+    frontier = util.PriorityQueue()
+    frontier.push(initialState, heuristic(initialState, problem))
+    explored = set()
+    nodeParentActionCost = {initialState: (None, None, 0)}
+    while not frontier.isEmpty():
+        node = frontier.pop()
+        if problem.isGoalState(node):
+            path = []
+            while nodeParentActionCost[node][0] != None:
+                path[:0] = [nodeParentActionCost[node][1]]
+                node = nodeParentActionCost[node][0]
+            return path
+        explored.add(node)
+        for child, action, cost in problem.getSuccessors(node):
+            # The only difference with the UCS algorithm is that on the new path cost it adds 
+            # The heuristic cost to the calculation, the rest are the same
+            if child not in explored:
+                if child not in nodeParentActionCost:
+                    nodeParentActionCost[child] = (node, action, nodeParentActionCost[node][2] + cost)
+                    frontier.push(child, nodeParentActionCost[node][2] + cost + heuristic(child, problem))
+                elif nodeParentActionCost[node][2] + cost + heuristic(child, problem) < nodeParentActionCost[child][2]:
+                    nodeParentActionCost[child] = (node, action, nodeParentActionCost[node][2] + cost)
+                    frontier.update(child, nodeParentActionCost[node][2] + cost + heuristic(child, problem))
+    return []
 
 
 # Abbreviations
